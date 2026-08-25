df51e6ecdcb71d2fd9bac8bc7f7e812c1ea2091d
Aadarsh Shrivastav <aadarshshrivastav01@gmail.com>
2026-08-25 10:43:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "./context/Theme/ThemeContext";
-import "./index.css"; // tailwind
+import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
